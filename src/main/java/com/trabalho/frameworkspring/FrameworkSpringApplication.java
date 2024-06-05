ede453c94fa71bae84c6d2c7f2e7559f1310ca1d
Gabriel Pinto Andrade <pintoandradegabriel@gmail.com>
2024-06-04 22:35:26 -0300

    Configurações iniciais

---
+++ src/main/java/com/trabalho/frameworkspring/FrameworkSpringApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.frameworkspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabalho/frameworkspring/FrameworkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.frameworkspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
